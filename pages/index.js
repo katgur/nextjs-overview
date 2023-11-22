cf7bf45aa9f6e5cd6f3c317623773392c0ebc376
Kate <mitsutanaka29@gmail.com>
2023-11-22 21:42:21 +0300

    added assets

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 export default function Home() {
   return (
-    <h1>
-      Read <Link href="/posts/first-post">this page!</Link>
-    </h1>
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+        <Link href="/posts/first-post">My first post</Link>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a target="_blank" href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
   );
 }
